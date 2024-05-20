67b08b4fe1584bbe371a1684768e6972965bc94f
Gustavo Franco da Silveira <gusfrancos@gmail.com>
2024-05-20 19:48:06 -0300

    Primeira versao funcionando

--- flutter_gymapp/windows/flutter/ephemeral/generated_config.cmake
+++ flutter_gymapp/windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
-file(TO_CMAKE_PATH "C:\\Gustavo\\Desenvolvimento\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Gustavo\\Desenvolvimento\\WORKSPACE_FLUTER\\flutter-gymapprepository\\flutter_gymapp" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Gustavo\\WorkSpaceFlutter\\flutter-gymapprepository\\flutter_gymapp" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -10,15 +10,15 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
-  "FLUTTER_ROOT=C:\\Gustavo\\Desenvolvimento\\flutter"
-  "PROJECT_DIR=C:\\Gustavo\\Desenvolvimento\\WORKSPACE_FLUTER\\flutter-gymapprepository\\flutter_gymapp"
-  "FLUTTER_ROOT=C:\\Gustavo\\Desenvolvimento\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Gustavo\\Desenvolvimento\\WORKSPACE_FLUTER\\flutter-gymapprepository\\flutter_gymapp\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Gustavo\\Desenvolvimento\\WORKSPACE_FLUTER\\flutter-gymapprepository\\flutter_gymapp"
-  "FLUTTER_TARGET=C:\\Gustavo\\Desenvolvimento\\WORKSPACE_FLUTER\\flutter-gymapprepository\\flutter_gymapp\\lib\\main.dart"
-  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Gustavo\\WorkSpaceFlutter\\flutter-gymapprepository\\flutter_gymapp"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Gustavo\\WorkSpaceFlutter\\flutter-gymapprepository\\flutter_gymapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Gustavo\\WorkSpaceFlutter\\flutter-gymapprepository\\flutter_gymapp"
+  "FLUTTER_TARGET=C:\\Gustavo\\WorkSpaceFlutter\\flutter-gymapprepository\\flutter_gymapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\Gustavo\\Desenvolvimento\\WORKSPACE_FLUTER\\flutter-gymapprepository\\flutter_gymapp\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Gustavo\\WorkSpaceFlutter\\flutter-gymapprepository\\flutter_gymapp\\.dart_tool\\package_config.json"
 )
